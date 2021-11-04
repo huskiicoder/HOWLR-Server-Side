f882921c36cd8c841edb01f1177f8892da74d1ee
etrobinson <erobins2@uw.edu>
2021-11-03 20:58:41 -0700

    Adeleted comments from confirm.js after the email verification commit.

--- routes/confirm.js
+++ routes/confirm.js
@@ -23,46 +23,4 @@ router.get('/', (request, response) => {
     }).catch((e) => console.log("error", e))
 })
 
-
-    // pool.query(theQuery, request.params.token)
-    //         .then(result => {
-    //             console.log('second')
-    //             // There is a match, confirm the email address
-    //             let verificationQuery = "UPDATE MEMBERS SET verification = 1 WHERE confirmCode = $1"
-    //             pool.query(verificationQuery, request.params.token)
-    //                 .then(result2 => {
-    //                     console.log('third')
-    //                     // delete the confirmation code for that user in the database
-    //                     let deleteQuery = "UPDATE Members SET confirmCode = -1 WHERE confirmCode = $1"
-    //                     pool.query(deleteQuery, request.params.token)
-    //                         .then(result3 => {
-    //                             // all done, email confirmed!
-    //                             console.log('Email confirmed!')
-    //                             response.status(201).send({
-    //                                 success: true,
-    //                             })
-    //                         })
-    //                 })
-    //         })
-//             .catch((error) => {
-//                 //log the error
-//                 // console.log(error)
-//                 if (error.constraint == "members_username_key") {
-//                     response.status(400).send({
-//                         message: "Username exists"
-//                     })
-//                 } else if (error.constraint == "members_email_key") {
-//                     response.status(400).send({
-//                         message: "Email exists"
-//                     })
-//                 } else {
-//                     console.log('register')
-//                     response.status(400).send({
-//                         message: "other error, see detail",
-//                         detail: error.detail
-//                     })
-//                 }
-//             })
-// })
-
 module.exports = router
