4342444dd72c73e910a826bfa71b03d294c35965
nhong10 <natalienguyen1110@gmail.com>
2021-11-23 12:02:35 -0800

    changed url for contact to get list of friends

--- routes/contacts.js
+++ routes/contacts.js
@@ -19,8 +19,8 @@ let isStringProvided = validation.isStringProvided
  * @apiName PostContacts
  * @apiGroup Contacts
  * 
- * @apiParam {String} member id A
- * @apiParam {String} member id B
+ * @apiParam {String} username A
+ * @apiParam {String} username B
  * 
  * @apiSuccess (Success 201) {boolean} success true when the member id is inserted
  * @apiSuccess (Success 201) {String} message the inserted member id
@@ -32,7 +32,7 @@ let isStringProvided = validation.isStringProvided
  * @apiUse JSONError
  */ 
  router.post("/", (request, response, next) => {
-    if (!isStringProvided(request.body.memberIdA) && !isStringProvided(request.body.memberIdA)) {
+    if (!isStringProvided(request.body.usernameA) && !isStringProvided(request.body.usernameB)) {
         response.status(400).send({
             message: "Missing required information"
         })
@@ -40,48 +40,50 @@ let isStringProvided = validation.isStringProvided
         next()
     }
 }, (request, response, next) => {
-    //validate memberid A exists in the chat
-    let query = 'SELECT * FROM Members WHERE MemberId=$1'
-    let values = [request.decoded.memberIdA]
+    //validate email exists AND convert it to the associated memberId
+    let query = 'SELECT MemberID FROM MEMBERS WHERE Email=$1'
+    let values = [request.params.usernameA]
 
     pool.query(query, values)
         .then(result => {
-            if (result.rowCount > 0) {
-                next()
-            } else {
-                response.status(400).send({
-                    message: "user A not in member"
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "email not found"
                 })
+            } else {
+                request.params.usernameA = result.rows[0].memberid
+                next()
             }
         }).catch(error => {
             response.status(400).send({
-                message: "SQL Error on member in chat check",
+                message: "SQL Error",
                 error: error
             })
-})
-}, (request, response, next) => {
-    //validate memberid B exists in the chat
-    let query = 'SELECT * FROM Members WHERE MemberId=$1'
-    let values = [request.decoded.memberIdB]
+        })
+},(request, response, next) => {
+    //validate email exists AND convert it to the associated memberId
+    let query = 'SELECT MemberID FROM MEMBERS WHERE Email=$1'
+    let values = [request.params.usernameB]
 
     pool.query(query, values)
         .then(result => {
-            if (result.rowCount > 0) {
-                next()
-            } else {
-                response.status(400).send({
-                    message: "user B not in member"
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "email not found"
                 })
+            } else {
+                request.params.usernameB = result.rows[0].memberid
+                next()
             }
         }).catch(error => {
             response.status(400).send({
-                message: "SQL Error on member in chat check",
+                message: "SQL Error",
                 error: error
+            })
         })
-})
 }, (request, response) => {
-    const theQuery = "INSERT INTO CONTACTS(MemberID_A, MemberID_B) VALUES ($1, $2) RETURNING *"
-    const values = [request.body.memberIdA, request.body.memberIdB]
+    const theQuery = `INSERT INTO CONTACTS(MemberID_A, MemberID_B) VALUES($1,$2) RETURN ContactID`
+    const values = [request.body.usernameA, request.body.usernameB]
 
     pool.query(theQuery, values)
         .then(result => {
@@ -96,110 +98,47 @@ let isStringProvided = validation.isStringProvided
                 error: err
             })    
         })
-},
+})
+
 
 /**
- * @api {get} /contacts/:primaryKey? Request to get the contact in a contact list
+ * @api {get} /contacts/:email Request to get the contact in a contact list
  * @apiName GetContacts
  * @apiGroup Contacts
  * 
  * @apiHeader {String} authorization Valid JSON Web Token JWT
  * 
- * @apiParam {Number} primaryKey the contact to look up. 
+ * @apiParam {Number} email the contact to look up. 
  * 
  * @apiSuccess {Number} rowCount the number of contacts returned
  * @apiSuccess {Object[]} contacts List of contacts in the a contact list
  * 
- * @apiError (404: ChatId Not Found) {String} message "Primary Key Not Found"
- * @apiError (400: Invalid Parameter) {String} message "Malformed parameter. primaryKey must be a number" 
+ * @apiError (404: Contact Id Not Found) {String} message "Email Not Found"
  * @apiError (400: Missing Parameters) {String} message "Missing required information"
  * 
  * @apiError (400: SQL Error) {String} message the reported SQL error details
  * 
  * @apiUse JSONError
  */ 
- router.get("/:primaryKey", (request, response, next) => {
-    //validate on missing or invalid (type) parameters
-    if (!request.params.primaryKey) {
+
+router.get("/:email", (request, response, next) => {
+    if (!request.params.email) {
         response.status(400).send({
             message: "Missing required information"
-        })
-    } else if (isNaN(request.params.primaryKey)) {
-        response.status(400).send({
-            message: "Malformed parameter. chatId must be a number"
         })
     } else {
         next()
     }
-},  (request, response) => {
-    //validate contact id exists
-    let query = 'SELECT * FROM CONTACTS WHERE PrimaryKey=$1'
-    let values = [request.params.primaryKey]
+},  (request, response, next) => {
+    //validate email exists
+    let query = 'SELECT * FROM MEMBERS WHERE Email=$1'
+    let values = [request.params.email]
 
     pool.query(query, values)
         .then(result => {
             if (result.rowCount == 0) {
                 response.status(404).send({
-                    message: "Contact ID not found"
-                })
-            } else {
-                response.send({
-                    rowCount : result.rowCount,
-                    rows: result.rows
-                })
-            }
-        }).catch(error => {
-            response.status(400).send({
-                message: "SQL Error",
-                error: error
-            })
-        })
-}))
-
-/**
- * @api {delete} /contacts/:primaryKey?/ Request delete a contact from a contact list
- * @apiName DeleteCcontact
- * @apiGroup Contacts
- * 
- * @apiDescription Does not delete the memberid associated with the required JWT but 
- * instead deletes the contact based on the primary key.  
- * 
- * @apiParam {Number} primaryKey the contact id to delete the contact from a contact list
- * 
- * @apiSuccess {boolean} success true when the contact is deleted
- * 
- * @apiError (404: Chat Not Found) {String} message "primaryKey not found"
- * @apiError (400: Invalid Parameter) {String} message "Malformed parameter. primaryKey must be a number" 
- * @apiError (400: Duplicate Email) {String} message "contact not in contact list"
- * @apiError (400: Missing Parameters) {String} message "Missing required information"
- * 
- * @apiError (400: SQL Error) {String} message the reported SQL error details
- * 
- * @apiUse JSONError
- */ 
-router.delete("/:primaryKey", (request, response, next) => {
-    //validate on empty parameters
-    if (!request.params.primaryKey) {
-        response.status(400).send({
-            message: "Missing required information"
-        })
-    } else if (isNaN(request.params.primaryKey)) {
-        response.status(400).send({
-            message: "Malformed parameter. chatId must be a number"
-        })
-    } else {
-        next()
-    }
-}, (request, response, next) => {
-    //validate chat id exists
-    let query = 'SELECT * FROM CONTACTS WHERE PrimaryKey=$1'
-    let values = [request.params.primaryKey]
-
-    pool.query(query, values)
-        .then(result => {
-            if (result.rowCount == 0) {
-                response.status(404).send({
-                    message: "Contact ID not found"
+                    message: "Email not found"
                 })
             } else {
                 next()
@@ -210,12 +149,130 @@ router.delete("/:primaryKey", (request, response, next) => {
                 error: error
             })
         })
+    }, (request, response) => {
+        //Retrieve the members
+        let query = `SELECT Members.MemberID, Members.Username, Members.Lastname, Members.Firstname 
+                    FROM Members
+                    INNER JOIN Contacts ON Members.MemberId=Contacts.MemberId_B
+                    WHERE Contacts.MemberId_A=(Select MemberID from Members where email=$1) 
+                    and Verified=1`
+        let values = [request.params.email]
+        pool.query(query, values)
+            .then(result => {
+                response.send({
+                    rowCount : result.rowCount,
+                    rows: result.rows
+                })
+            }).catch(err => {
+                response.status(400).send({
+                    message: "SQL Error",
+                    error: err
+                })
+            })
+});
+
+/**
+ * @api {delete} /contacts/:username/:memberid Request delete a contact from a contact list
+ * @apiName DeleteCcontact
+ * @apiGroup Contacts
+ * 
+ * @apiDescription Does not delete the memberid associated with the required JWT but 
+ * instead deletes the contact based on the username and memberid.  
+ * 
+ * @apiParam {Number} memberid the member id to delete the contact from a contact list
+ * 
+ * @apiSuccess {boolean} success true when the contact is deleted
+ * 
+ * @apiError (404: Contact Not Found) {String} message "memberid not found"
+ * @apiError (400: Invalid Parameter) {String} message "Malformed parameter. memberid must be a number" 
+ * @apiError (400: Duplicate Email) {String} message "memberid not in contact list"
+ * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * 
+ * @apiError (400: SQL Error) {String} message the reported SQL error details
+ * 
+ * @apiUse JSONError
+ */ 
+router.delete("/:username/:memberid", (request, response, next) => {
+    if (!request.params.username || !request.params.memberid) {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    } else if (isNaN(request.params.memberid)) {
+        response.status(400).send({
+            message: "Malformed parameter. memberid must be a number"
+        })
+    } else {
+        next()
+    }
+}, (request, response, next) => {
+    //validate chat id exists
+    let query = 'SELECT * FROM Contacts WHERE memberid=$1'
+    let values = [request.params.memberid]
+
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "MemberId not found"
+                })
+            } else {
+                next()
+            }
+        }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: error
+            })
+        })
+}, (request, response, next) => {
+    //validate email exists AND convert it to the associated memberId
+    let query = 'SELECT MemberID FROM Members WHERE Username=$1'
+    let values = [request.params.username]
+
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "email not found"
+                })
+            } else {
+                request.params.username = result.rows[0].memberid
+                next()
+            }
+        }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: error
+            })
+        })
+}, (request, response, next) => {
+        //validate email exists in the chat
+        let query = 'SELECT * FROM Contacts WHERE MemberId_A=$1 AND MemberId_B=$2'
+        let values = [request.params.username, request.params.memberid]
+    
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount > 0) {
+                    next()
+                } else {
+                    response.status(400).send({
+                        message: "Contact doesnot exist"
+                    })
+                }
+            }).catch(error => {
+                response.status(400).send({
+                    message: "SQL Error",
+                    error: error
+                })
+            })
+
 }, (request, response) => {
-    //Delete the primaryKey from the contacts
-    let insert = `DELETE FROM CONTACTS
-                  WHERE PrimaryKey=$1
+    //Delete the memberId from the chat
+    let insert = `DELETE FROM Contacts
+                  WHERE MemberId_A=$1
+                  AND MemberId_B=$2
                   RETURNING *`
-    let values = [request.params.primaryKey]
+    let values = [request.params.username, request.params.memberid]
     pool.query(insert, values)
         .then(result => {
             response.send({
